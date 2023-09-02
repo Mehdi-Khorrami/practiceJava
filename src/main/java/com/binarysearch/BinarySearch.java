fe11c7dd62eb10edaba7e1312393d10213786d1f
mehi <mehdi.khorrami78@gmail.com>
2023-09-02 10:35:34 +0330

    write binarysearch method

---
+++ src/main/java/com/binarysearch/BinarySearch.java
@@ -0,0 +1,49 @@
+package com.binarysearch;
+
+public class BinarySearch {
+
+    public static Integer binarySearch(int[] arr, int key) {
+        if (arr.length == 0) {
+            return null;
+        }
+        sort(arr);
+        int mid = arr.length / 2;
+        int first = arr[0];
+        int last = arr[arr.length - 1];
+
+
+        while (first <= last) {
+            if ( arr[mid] < key ) {
+                first = arr[mid++];
+            } else if (arr[mid] > key) {
+                last = arr[mid--];
+            } else {
+                break;
+            }
+        }
+
+        return mid;
+    }
+
+
+    public static int[] sort(int[] arr) {
+        int extra;
+        boolean flag;
+        for (int i = 0; i < arr.length; i++) {
+            flag = false;
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    extra = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = extra;
+
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                break;
+            }
+        }
+        return arr;
+    }
+}
---
+++ src/main/java/com/binarysearch/Main.java
@@ -0,0 +1,10 @@
+package com.binarysearch;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = new int[]{ 22 , 20 , 15 , 1 ,  9  , 12 , 5 , 25  , 46 };
+        System.out.println(BinarySearch.binarySearch(a, 20));
+    }
+}
