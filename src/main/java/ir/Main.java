a13c10f3210800078458e9f6bb2ad2417587c7d5
mehdi.khorrami <mehdi.khorrami78@gmail.com>
2023-09-02 15:37:24 +0330

    add operationArray class

--- src/main/java/ir/Main.java
+++ src/main/java/ir/Main.java
@@ -2,22 +2,75 @@ package ir;
 
 import ir.arrayindex.ArrayIndex;
 import ir.binarysearch.BinarySearch;
+import ir.operationinarray.OperationArray;
 import ir.pattern.PrintPattern;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Integer[] a = new Integer[]{ 22 , 20 , 15 , 1 ,  9  , 12 , 5 , 25  , 46 };
+        //int[] a = new int[]{ 22 , 20 , 15 , 1 ,  9  , 12 , 5 , 25  , 46 };
 
         //PrintPattern.printStar(100);
 
         //System.out.println(BinarySearch.binarySearch(a, 20));
 
-        ArrayIndex.remove(a, 0);
+        //ArrayIndex.remove(a, 0);
 
-        System.out.println(Arrays.toString(a));
+        //System.out.println(Arrays.toString(a));
+
+
+
+        ArrayList<Integer> arr = scanner();
+        doOperation(arr);
+
+
+
+
+    }
+
+    private static void doOperation(ArrayList<Integer> arr) {
+        boolean shouldBreak = false;
+        while (!shouldBreak) {
+            System.out.println("Select one of below commands");
+            System.out.println("0) Quit");
+            System.out.println("1) Average");
+            System.out.println("2) Sum");
+
+            Scanner scanner = new Scanner(System.in);
+            int i = scanner.nextInt();
+            switch (i) {
+                case 0 -> {
+                    shouldBreak = true;
+                }
+                case 1 -> {
+                    double avg = OperationArray.average(arr);
+                    System.out.println("Average is " + avg);
+                }
+                case 2 -> {
+                    Long sum = OperationArray.sum(arr);
+                    System.out.println("Sum is " + sum);
+                }
+                default -> System.err.println("Invalid input");
+            }
+        }
+    }
+
+    private static ArrayList<Integer> scanner() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input Array items ");
+        String s = scanner.nextLine();
+        String[] strings = s.split(" ");
+        ArrayList<Integer> ints = new ArrayList<>();
+
+        for (String string : strings) {
+            ints.add(Integer.parseInt(string));
+        }
+        System.out.println("Input is " + ints);
+        return ints;
     }
 }
 
---
+++ src/main/java/ir/operationinarray/OperationArray.java
@@ -0,0 +1,179 @@
+package ir.operationinarray;
+
+
+import ir.binarysearch.BinarySearch;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class OperationArray {
+
+    public static Long sum(ArrayList<Integer> arr){
+        if ( arr == null || arr.isEmpty()  ) {
+            return null;
+        }
+
+        Long sum = 0L;
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static double average(ArrayList<Integer> arr){
+        return sum(arr) / arr.size();
+    }
+
+    public static int[] remove(int[] arr ,  int index){
+        if(arr.length == 0 ) {
+            System.out.println("array is null");
+        }
+        arr[index] = 0;
+        return shiftArray(arr);
+    }
+    public static int[] shiftArray(int[] arr){
+        int[] result = new int[arr.length - 1];
+        Integer index = null;
+        for (int i = 0 ; i < arr.length ; i++){
+            if(arr[i] == 0){
+                index = i ;
+                break;
+            }
+        }
+
+        if (index == null){
+            return null;
+        }
+
+        for (int i = index ; i < arr.length - 1; i++){
+            arr[i] = arr[i+1];
+        }
+
+        for (int i =0 ; i < result.length ; i++){
+            result[i] = arr[i];
+        }
+
+        return result;
+
+    }
+
+    public static Integer max(int[] a) {
+        if (a == null || a.length == 0) {
+            return null;
+        }
+        int max = Integer.MIN_VALUE;
+        for (int j : a) {
+            if (j > max) {
+                max = j;
+            }
+        }
+        return max;
+    }
+
+    public static Integer min(int[] a) {
+        if (a == null || a.length == 0) {
+            return null;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int j : a) {
+            if (j < min) {
+                min = j;
+            }
+        }
+        return min;
+    }
+
+    public static int[] of(int... a) {
+        return a;
+    }
+
+    public static int[] randomArray(int n) {
+        if (n <= 0) {
+            return null;
+        }
+        int[] result = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            result[i] = random.nextInt();
+        }
+        return result;
+    }
+
+    public static int compare(int[] a, int[] b) {
+        int n = min(of(a.length, b.length));
+        for (int i = 0; i < n; i++) {
+            if (a[i] > b[i]) {
+                return 1;
+            } else if (a[i] < b[i]) {
+                return -1;
+            }
+        }
+        return Integer.compare(a.length, b.length);
+    }
+
+    public static int[] sort(int[] arr) {
+        return BinarySearch.sort(arr);
+    }
+
+    public static int[] subArray(int[] a, int from, int to) {
+        if (a.length == 0 || to < from || from < 0 || from >= a.length || to >= a.length) {
+            return null;
+        }
+
+        int[] result = new int[to - from + 1];
+        int index = 0;
+        for (int i = from; i <= to; i++) {
+            result[index++] = a[i];
+        }
+        return result;
+    }
+
+
+    public static boolean isEqual(int[] a, int[] b) {
+        if (a.length != b.length) {
+            return false;
+        }
+        if (a.length == 0) {
+            return false;
+        }
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] reverse(int[] a) {
+        int[] result = new int[a.length];
+        for (int i = a.length - 1; i >= 0; i--) {
+            result[a.length - 1 - i] = a[i];
+        }
+        return result;
+    }
+
+    public static int indexOf(int[] a, int item) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean contains(int[] a, int item) {
+        return indexOf(a, item) >= 0;
+    }
+
+    private static boolean isPrime(int i) {
+        int count = 0;
+        for (int j = 1; j <= (i / 2); j++) {
+            if (i % j == 0) {
+                count++;
+            }
+        }
+        return count == 1;
+    }
+
+}
